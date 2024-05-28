180af8eeb80d9879318e9f9735d51820f587b42a
Easton <easton@localhost.localdomain>
2024-05-29 07:01:21 +0800

    数据预处理学习 2024.5.28

--- 动手学深度学习/数据预处理.py
+++ 动手学深度学习/数据预处理.py
@@ -18,5 +18,31 @@ print(data)
 
 # 处理缺失值
 inputs, outputs = data.iloc[:, 0:2], data.iloc[:, 2]
-inputs = inputs.fillna(inputs.mean())
-print(inputs.mean())
+inputs['NumRooms'] = inputs['NumRooms'].fillna(inputs['NumRooms'].mean())
+print(inputs)
+
+inputs = pd.get_dummies(inputs, dummy_na=True)
+print(inputs)
+
+# 转换为张量格式
+X = torch.tensor(inputs.to_numpy(dtype=float))
+Y = torch.tensor(outputs.to_numpy(dtype=float))
+print(X, Y)
+
+# 作业1：删除缺失值最多的列
+rows = data.iloc[:, :]
+missing_value_counts = rows.isnull().sum(axis=0)# 此处axis=0代表对所有的行操作求和
+row_with_most_missing_values = missing_value_counts.idxmax()
+rows = rows.drop(row_with_most_missing_values, axis=1)# 此处axis=1代表删除对应索引的列
+print(rows)
+
+# 作业2:将预处理后的数据集转化为张量格式
+rows = rows.fillna(rows.mean())
+rows_tensor = torch.tensor(rows.to_numpy(dtype=float))
+print(rows_tensor)
+
+# 如果采用删除法处理缺失值（都会删除）
+rows_delete = data.iloc[:, :].dropna()
+print(rows_delete)
+rows_delete_tensor = torch.tensor(rows_delete.to_numpy(dtype=float))
+print(rows_delete_tensor)
